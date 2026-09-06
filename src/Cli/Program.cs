cc0855f9f6238c0c891421ee01612548f4209bfe
drack <artemsenuv@gmail.com>
2026-09-06 20:19:28 +0300

    lab01: solution CrossApp, Cli, Domain: Order

---
+++ src/Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("DrackDrop");
+Console.WriteLine("Студент: Сенів Артем, група ФЕІ-35");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"ОС (OSDescription)  : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment)    : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR)   : {Environment.Version}");
+Console.WriteLine($"Runtime             : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку  : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог    : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Замовлення");
+Console.WriteLine("Призначення: оформлення замовлень і підрахунок сум.");
